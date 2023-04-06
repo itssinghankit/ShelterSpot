c7238d3f1f6732950c561278645058d3b773d862
itssinghankit <singhankit.kr@gmail.com>
2023-04-06 22:41:16 +0530

    customer hotel details map

--- app/src/main/java/com/example/shelterspot/CHotelDetails.kt
+++ app/src/main/java/com/example/shelterspot/CHotelDetails.kt
@@ -1,8 +1,8 @@
 package com.example.shelterspot
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.shelterspot.databinding.ActivityChotelDetailsBinding
 import com.google.firebase.database.DatabaseReference
@@ -18,6 +18,7 @@ class CHotelDetails : AppCompatActivity() {
         binding= ActivityChotelDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val userId=intent.getStringExtra("userId").toString()
+
 
         database=FirebaseDatabase.getInstance().getReference("hotels")
         database.child(userId).get().addOnSuccessListener {
@@ -39,7 +40,11 @@ class CHotelDetails : AppCompatActivity() {
             startActivity(intent)
         }
 
-
+        //after book button is clicked
+        binding.book.setOnClickListener{
+            val intent=Intent(this,HotelBookingDetails::class.java)
+            startActivity(intent)
+        }
 
     }
 
